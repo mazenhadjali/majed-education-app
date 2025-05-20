cc2afcff43858df6aea9f1927ee40640bb9db801
mazenhadjali <hadjalimazen@gmail.com>
2025-05-20 04:17:02 +0100

    adding titles correctly

--- src/pages/AppList.jsx
+++ src/pages/AppList.jsx
@@ -3,13 +3,13 @@ import { P1, P2, P3, P4, P5, P6, P7 } from '../constants'
 
 function AppList() {
     const exercises = [
-        { id: 1, name: 'تمرين 1', path: P1 },
-        { id: 2, name: 'تمرين 2', path: P2 },
-        { id: 3, name: 'تمرين 3', path: P3 },
-        { id: 4, name: 'تمرين 4', path: P4 },
-        { id: 5, name: 'تمرين 5', path: P5 },
-        { id: 6, name: 'تمرين 6', path: P6 },
-        { id: 7, name: 'تمرين 7', path: P7 },
+        { id: 1, name: 'مَاجِدٌ وَحَنونُ فِي مَمْلَكَةِ الجُمَلِ العَجِيبَةِ', path: P1 },
+        { id: 2, name: 'مغامرة المركّب الإضافي في قرية بَيْلَسَان الساحرة', path: P2 },
+        { id: 3, name: 'قصة عائلة كان وأخواتها', path: P3 },
+        { id: 4, name: 'مغامرة ماجد وحنون في الغابة المضيئة', path: P4 },
+        { id: 5, name: 'مَاجِدٌ وَحَنُونٌ وَالْمَفْعُولُ المَطْلَقُ', path: P5 },
+        { id: 6, name: 'سر المفعول لأجله في قرية الأضواء السحرية', path: P6 },
+        { id: 7, name: 'مغامرة الأفعال المعتلة وسرها', path: P7 },
     ]
     return (
         <div>
